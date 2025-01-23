37721d6273e35509e71e0e1507229be616b54f8a
pynat <natlu@gmx.de>
2025-01-23 09:24:50 +0100

    final model

--- train.py
+++ train.py
@@ -1,27 +1,55 @@
 import pickle
+import numpy as np
 import xgboost as xgb
-from sklearn.metrics import roc_auc_score
-from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from math import sqrt
 
-# loading features
-with open('features.pkl', 'rb') as file:
-    X_train, X_val, y_train, y_val = pickle.load(file)
+# load features
+try:
+    with open('features.pkl', 'rb') as file:
+        X_train, X_val, y_train_log, y_val_log = pickle.load(file)
+except FileNotFoundError:
+    raise FileNotFoundError("The file 'features.pkl' was not found. Make sure it is in the correct directory.")
 
-# loading model
-with open('xgboost_model.pkl', 'rb') as file:
-    model_xgb_final = pickle.load(file)
+# best parameters 
+best_xgb_params = {
+    'eta':  0.25,
+    'max_depth': 5,
+    'min_child_weight': 1, 
+    'objective': 'reg:squarederror',
+    'eval_metric': 'rmse',
+    'nthread': 8,
+    'seed': 1,
+    'verbosity': 1,
+}
 
-# retrain model
-model_xgb_final.fit(X_train, y_train)
 
-# evaluate the model on validation
-val_auc = roc_auc_score(y_val, model_xgb_final.predict_proba(X_val)[:, 1])
-print(f"Validation ROC-AUC for final model: {val_auc}")
+# train final model
+model_xgb_final = xgb.XGBRegressor(**best_xgb_params)
+model_xgb_final.fit(X_train, y_train_log)
+
+# predictions on validation set
+y_pred_val_log = model_xgb_final.predict(X_val)
+y_pred_val = np.expm1(y_pred_val_log)
+y_val_actual = np.expm1(y_val_log)
+
+# evaluate
+rmse = sqrt(mean_squared_error(y_val_actual, y_pred_val))
+mse = mean_squared_error(y_val_actual, y_pred_val)
+mae = mean_absolute_error(y_val_actual, y_pred_val)
+r2 = r2_score(y_val_actual, y_pred_val)
+
+# print
+print(f"Root Mean Squared Error (RMSE): {rmse:.4f}")
+print(f"Mean Squared Error (MSE): {mse:.4f}")
+print(f"Mean Absolute Error (MAE): {mae:.4f}")
+print(f"R-squared (R2): {r2:.4f}")
+
 
 # save final model
-with open('final_xgboost_model.pkl', 'wb') as file:
-    pickle.dump(model_xgb_final, file)
-
-print("Final model trained and saved as 'final_xgboost_model.pkl'")
-
-
+try:
+    with open('final_xgboost_model.pkl', 'wb') as file:
+        pickle.dump(model_xgb_final, file)
+    print("Final model trained and saved as 'final_xgboost_model.pkl'")
+except Exception as e:
+    print(f"Error saving the model: {e}")
